db4fe1f35e198f95c684657b0d617760ac6411af
Alexey Khrushch <alexeykhr@outlook.com>
2021-02-10 11:30:01 +0200

    chore(web): replace then to finally

--- web/src/scripts.js
+++ web/src/scripts.js
@@ -7,8 +7,8 @@ import Error from './Error'
 const td = new TableData(new Fetcher(), new Error())
 
 td.fetchStats()
-  .then(td.show.bind(td))
-  .then(() => {
+  .finally(td.show.bind(td))
+  .finally(() => {
     const urlParams = new URLSearchParams(window.location.search)
     const timeout = +urlParams.get('timeout') || 0
 
